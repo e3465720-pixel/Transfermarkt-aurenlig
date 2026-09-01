0247ae1886526692e10e4a02934965e34d62d1b4
e3465720-pixel <e3465720@gmail.com>
2026-09-01 16:45:58 +0300

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
